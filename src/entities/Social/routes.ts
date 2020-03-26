9090d54b39d5acefece74d39cdea7620f0b9f9cf
Fede <i@2fd.me>
2020-03-26 12:06:47 -0300

    fix: publish only approved events

--- src/entities/Social/routes.ts
+++ src/entities/Social/routes.ts
@@ -19,7 +19,7 @@ export async function injectSocialTag(req: WithSocialUserAgent, res: Response, n
     return next()
   }
 
-  const event = await Event.findOne<EventAttributes>({ id: req.query.event })
+  const event = await Event.findOne<EventAttributes>({ id: req.query.event, approved: true })
   if (!event) {
     return next()
   }
