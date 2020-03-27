0ea751c601d18d35832034c4b3829ee536f376cb
Fede <i@2fd.me>
2020-03-26 21:18:06 -0300

    feat: expose image data

--- src/entities/Route/routes.ts
+++ src/entities/Route/routes.ts
@@ -1,7 +1,11 @@
 import { Router, RouterOptions } from 'express'
 import handle from './handle';
+import env from 'decentraland-gatsby/dist/utils/env';
 
 export type RouterHandler = (router: Router) => void
+
+const IMAGE = env('IMAGE', 'unknown:unknown')
+const [image, version] = IMAGE.split(':')
 
 export default function routes(handle: RouterHandler, options: RouterOptions = {}): Router {
   const router = Router(options)
@@ -11,6 +15,10 @@ export default function routes(handle: RouterHandler, options: RouterOptions = {
 
 export function status() {
   return routes((router) => {
-    router.get('/status', handle(async () => new Date()))
+    router.get('/status', handle(async () => ({
+      image,
+      version,
+      timestamp: new Date()
+    })))
   })
 }
