44f33eaf593c05900ad8b5dd602995d9af986f58
Fede <i@2fd.me>
2020-04-06 10:00:25 -0300

    feat: show loader when there are no events

--- src/store.ts
+++ src/store.ts
@@ -2,5 +2,5 @@ import EntityStore from "decentraland-gatsby/dist/utils/EntityStore";
 import { EventAttributes } from "./entities/Event/types";
 
 export default {
-  event: new EntityStore<EventAttributes>({ initialState: {} })
+  event: new EntityStore<EventAttributes>({ initialState: { loading: true } })
 }
