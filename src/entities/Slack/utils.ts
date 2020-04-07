fd003ab920d629752d2d703fb377a29f0878877e
Fede <i@2fd.me>
2020-04-07 14:24:41 -0300

    fix: error notification

--- src/entities/Slack/utils.ts
+++ src/entities/Slack/utils.ts
@@ -88,7 +88,7 @@ export async function notifyEventError(user: Avatar, error: RequestError) {
     NEXT_ERROR_AT = Date.now() + SEND_CLOUD_DOWN
     console.log(`sending error to slack`)
     const userName = user.name || 'Guest'
-    const userContact = user.email ? `<mailto:${user.email}|${userName}[${user.ethAddress}]>` : `${userName}[${user.ethAddress}]`
+    const userContact = user.email ? `<mailto:${user.email}|${userName} [${user.ethAddress}]>` : `${userName} [${user.ethAddress}]`
     const errorDetails = '```' + JSON.stringify({ message: error.message, statusCode: error.statusCode, data: error.data || null }, null, 2) + '```'
     await sendToSlack({
       // :x:
