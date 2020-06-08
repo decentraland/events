b04a454e8c2a5dd69944f0f0b89b80d9e710b9d9
Fede <i@2fd.me>
2020-06-07 22:25:11 -0300

    style: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,20 @@
 import React from "react"
-
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
+
+import { Container } from "decentraland-ui/dist/components/Container/Container"
+import Title from "decentraland-gatsby/dist/components/Text/Title"
+import Paragraph from "decentraland-gatsby/dist/components/Text/Paragraph"
 
 const NotFoundPage = (props: any) => (
   <Layout {...props} >
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p> You just hit a route that doesn &#39; t exist...the sadness.</p>
+    <Container>
+      <div style={{ minHeight: '75vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <Title>NOT FOUND</Title>
+        <Paragraph> You just hit a route that doesn&#39;t exist...</Paragraph>
+      </div>
+    </Container>
   </Layout>
 )
 
