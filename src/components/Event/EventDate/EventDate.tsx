41892e1b8a701e7bcc2964649870619fe9a645eb
Fede <i@2fd.me>
2020-06-29 15:51:14 -0300

    fix: event finish date

--- src/components/Event/EventDate/EventDate.tsx
+++ src/components/Event/EventDate/EventDate.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { toMonthName } from 'decentraland-gatsby/dist/components/Date/utils'
 import TokenList from 'decentraland-gatsby/dist/utils/TokenList'
 import { SessionEventAttributes } from '../../../entities/Event/types'
-
 import './EventDate.css'
-import { start } from 'repl';
 
 export type EventDateProps = React.HTMLProps<HTMLDivElement> & {
   event: SessionEventAttributes
@@ -13,20 +11,21 @@ export type EventDateProps = React.HTMLProps<HTMLDivElement> & {
 export default function EventDate(props: EventDateProps) {
 
   const currentTime = Date.now()
-  const { next_start_at, duration } = props.event || { next_start_at: new Date(currentTime) }
-  const startDay = new Date(props.event.start_at)
+  const duration = props.event.duration || (1000 * 60 * 60)
+  const startAt = props.event.next_start_at || new Date(currentTime)
+  const finishAt = new Date(startAt.getTime() + duration)
+  const startDay = new Date(startAt)
   startDay.setUTCHours(0)
   startDay.setUTCMinutes(0)
   startDay.setUTCSeconds(0)
   startDay.setUTCMilliseconds(0)
   const previousStartDay = new Date(startDay.getTime())
   previousStartDay.setUTCDate(previousStartDay.getUTCDate() - 1)
-  const finishAt = new Date(next_start_at.getTime() + duration)
 
-  const isNow = currentTime >= next_start_at.getTime() && currentTime < finishAt.getTime()
-  const isToday = currentTime < next_start_at.getTime() && currentTime > startDay.getTime()
+  const isNow = currentTime >= startAt.getTime() && currentTime < finishAt.getTime()
+  const isToday = currentTime < startAt.getTime() && currentTime > startDay.getTime()
   const isTomorrow = currentTime < startDay.getTime() && currentTime > previousStartDay.getTime()
-  const date = toMonthName(next_start_at, { utc: true }) + ' ' + next_start_at.getUTCDate()
+  const date = toMonthName(startAt, { utc: true }) + ' ' + startAt.getUTCDate()
   return <div {...props} className={TokenList.join(['EventDate', props.className])}>
     {isNow && 'NOW'}
     {isToday && 'TODAY'}
