b3224bcb55825f4afb8ff86fa4c14ce788b50465
Fede <i@2fd.me>
2020-08-10 19:58:10 -0300

    chore: improve file handle

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import express, { Response, Request } from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
-import cache from 'apicache'
 import Manager from 'decentraland-gatsby/dist/entities/Job/job'
 import { listen } from 'decentraland-gatsby/dist/entities/Server/utils'
-import { status, logger, file, ddos } from 'decentraland-gatsby/dist/entities/Route/routes'
+import { status, logger, ddos, filesystem } from 'decentraland-gatsby/dist/entities/Route/routes'
 import database from './entities/Database/index'
 import events from './entities/Event/routes'
 import attendees from './entities/EventAttendee/routes'
@@ -15,7 +14,6 @@ import profileSettings, { verifySubscription, removeSubscription } from './entit
 import profileSubscription from './entities/ProfileSubscription/routes'
 import { SUBSCRIPTION_PATH, UNSUBSCRIBE_PATH } from './entities/ProfileSettings/types'
 import { notifyUpcomingEvents } from './entities/Event/cron'
-import Datetime from 'decentraland-gatsby/dist/utils/Datetime'
 import handle from 'decentraland-gatsby/dist/entities/Route/handle'
 import RequestError from 'decentraland-gatsby/dist/entities/Route/error'
 
@@ -23,6 +21,7 @@ const jobs = new Manager({ concurrency: 10 })
 jobs.cron('0 * * * * *', notifyUpcomingEvents)
 
 const app = express()
+app.set('x-powered-by', false)
 app.use(social)
 app.use('/api', [
   ddos(),
@@ -44,15 +43,7 @@ app.use('/api', [
 app.get(SUBSCRIPTION_PATH, verifySubscription)
 app.get(UNSUBSCRIBE_PATH, removeSubscription)
 
-const staticCache = cache.middleware(
-  7 * Datetime.Day,
-  (req: Request, res: Response) => {
-    return req.method === 'GET' && res.statusCode === 200
-  },
-  {}
-)
-app.use(staticCache, express.static('public', { maxAge: 1000 * 60 * 60 }))
-app.use(file(process.cwd() + '/public/404.html', 404))
+app.use(filesystem('public', '404.html'))
 
 Promise.resolve()
   .then(() => database.connect())
