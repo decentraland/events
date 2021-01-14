97bf7b3013aaabc8ec508557c62fbc4ec632a2a5
Fede <i@2fd.me>
2021-01-14 11:28:44 -0300

    chore: fix cluster name

--- .ci/helpers/ecs.ts
+++ .ci/helpers/ecs.ts
@@ -4,7 +4,7 @@ import { env } from "dcl-ops-lib/domain"
 import withCache from "dcl-ops-lib/withCache"
 
 export const getCluster = withCache(async () => {
-  const cluster = `${env}-name`
+  const cluster = `${env}-main`
   return new awsx.ecs.Cluster(cluster + "-ref", {
     cluster: aws.ecs.Cluster.get(cluster + "-ref-2", cluster),
   })
