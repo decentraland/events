5ddfa0bdcae1729e8c2af5c08c200237304b8fc1
Fede <i@2fd.me>
2021-01-30 22:37:20 -0300

    chore: upgrade deploy

--- Pulumi.ts
+++ Pulumi.ts
@@ -1,11 +1,11 @@
-import { resolve } from "path";
+// import { resolve } from "path";
 import { buildGatsby } from "decentraland-gatsby-deploy/dist/recepies/buildGatsby";
 const { gatsby } = require("./package.json");
 
 export = async function main() {
   return buildGatsby({
     name: 'events',
-    contentDirectory: resolve(__dirname, '../public'),
+    // contentSource: resolve(__dirname, '../public'),
     usePublicTLD: process.env['USE_PUBLIC_TLD'] === 'true',
     serviceImage: process.env['CI_REGISTRY_IMAGE'],
     servicePaths: [
