67e25b700c16f595e13f96432f8e81b6e2774f7f
frami <i@2fd.me>
2022-01-18 16:30:00 -0300

    fix: update events records

---
+++ src/migrations/1642533892903_update-events.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { SQL, table } from 'decentraland-gatsby/dist/entities/Database/utils';
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+import EventModel from '../entities/Event/model';
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+    UPDATE "${EventModel.tableName}"
+    SET "next_start_at" = "start_at"
+    WHERE "next_start_at" IS NULL;
+  `)
+
+  pgm.sql(`
+    UPDATE "${EventModel.tableName}"
+    SET "next_finish_at" = "next_start_at" + ("duration" * '1 millisecond'::interval)
+    WHERE "next_finish_at" IS NULL;`)
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {}
