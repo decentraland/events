9de18facae2c4cb33f1625efe60602e319b87ca4
frami <i@2fd.me>
2022-03-24 14:15:52 -0300

    fix: sw

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'src/',
+	globPatterns: [
+		'**/*.{ts,css,tsx,js,png,svg,json}'
+	],
+	swDest: 'static/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
