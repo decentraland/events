99f799e781a33ab633ee6da9a00c7cc62449bb09
frami <i@2fd.me>
2022-12-08 17:59:30 -0300

    refactor: sender and porter

--- src/entities/Notification/sender.ts
+++ src/entities/Notification/sender.ts
@@ -2,8 +2,8 @@ import Sender from "decentraland-gatsby/dist/entities/Mail/sender"
 import env, { requiredEnv } from "decentraland-gatsby/dist/utils/env"
 
 const SERVICE_DOMAIN = env("SERVICE_ORG_DOMAIN", "decentraland.org")
-const AWS_ACCESS_KEY = requiredEnv("AWS_ACCESS_KEY")
-const AWS_ACCESS_SECRET = requiredEnv("AWS_ACCESS_SECRET")
+const AWS_ACCESS_KEY = env("AWS_ACCESS_KEY")
+const AWS_ACCESS_SECRET = env("AWS_ACCESS_SECRET")
 const AWS_REGION = env("AWS_REGION", "us-east-1")
 
 export default new Sender({
