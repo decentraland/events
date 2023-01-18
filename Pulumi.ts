285d659020858b07015240a169dcbaf5a7158a1f
frami <i@2fd.me>
2023-01-18 10:08:06 -0300

    chore: service domain

--- Pulumi.ts
+++ Pulumi.ts
@@ -1,17 +1,22 @@
 // import { resolve } from "path";
+import { domain, publicDomain } from "dcl-ops-lib/domain"
 import { variable } from "decentraland-gatsby-deploy/dist/pulumi/env"
 import { buildGatsby } from "decentraland-gatsby-deploy/dist/recepies/buildGatsby"
 
 export = async function main() {
+  const usePublicTLD = process.env["USE_PUBLIC_TLD"] === "true"
+  const serviceDomain = `events-old.${usePublicTLD ? publicDomain : domain}`
+
   return buildGatsby({
     name: "events",
     // contentSource: resolve(__dirname, '../public'),
-    usePublicTLD: process.env["USE_PUBLIC_TLD"] === "true",
+    usePublicTLD,
     // this can't be enabled en order to support old links
     // contentRoutingRules: {
     //   '/en/*': '/$1'
     // },
     // serviceSource: ".",
+    serviceDomain,
     serviceImage: process.env["CI_REGISTRY_IMAGE"],
     serviceMemory: 1024,
     serviceEnvironment: [variable("NODE_ENV", "production")],
