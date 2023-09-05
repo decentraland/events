6d011d2a0ff76e9f985d3767f244060c7784eaeb
Florencia Barreto <florbarrlad@gmail.com>
2023-09-05 19:49:00 +0200

    feat: changed feature flag name

--- src/modules/features.ts
+++ src/modules/features.ts
@@ -1,5 +1,5 @@
 export enum Flags {
   ProfileSite = "dapps-profile",
   HideEventsInWorlds = "events-hide-events-in-worlds",
-  NewNavbarDropdown = "events-new-navbar-dropdown",
+  NewNavbarDropdown = "dapps-new-navbar-dropdown",
 }
