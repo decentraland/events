87c2d25cba90f1d51e863fd240318894057f6e8b
Lautaro Petaccio <lausuper@gmail.com>
2023-09-14 20:14:29 -0300

    fix: Update decentraland gatsby

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
+import { Logger } from "@ethersproject/logger"
 import { setupEnv } from "decentraland-gatsby/dist/utils/env"
 
+Logger.prototype.checkAbstract = function () {}
 import dev from "./dev.json"
 import local from "./local.json"
 import prod from "./prod.json"
