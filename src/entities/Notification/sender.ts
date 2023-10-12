5bc0c682587bf87d08abe4cdc58bb8be0c4330e6
Lautaro Petaccio <lausuper@gmail.com>
2023-10-12 18:47:07 -0300

    fix: Update credentials type

--- src/entities/Notification/sender.ts
+++ src/entities/Notification/sender.ts
@@ -1,5 +1,5 @@
 import Sender from "decentraland-gatsby/dist/entities/Mail/sender"
-import env, { requiredEnv } from "decentraland-gatsby/dist/utils/env"
+import env from "decentraland-gatsby/dist/utils/env"
 
 const SERVICE_DOMAIN = env("SERVICE_ORG_DOMAIN", "decentraland.org")
 const AWS_ACCESS_KEY = env("AWS_ACCESS_KEY")
@@ -7,8 +7,10 @@ const AWS_ACCESS_SECRET = env("AWS_ACCESS_SECRET")
 const AWS_REGION = env("AWS_REGION", "us-east-1")
 
 export default new Sender({
-  accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_ACCESS_SECRET,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY ?? "",
+    secretAccessKey: AWS_ACCESS_SECRET ?? "",
+  },
   region: AWS_REGION,
   source: `Decentraland Events <hello@${SERVICE_DOMAIN}>`,
   bulk: process.env.NODE_ENV === "production",
