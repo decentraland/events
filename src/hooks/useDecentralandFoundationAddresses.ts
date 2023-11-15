ff7494a67d88b7d2d063d963442e3df062c75e64
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-11-15 16:09:08 -0300

    fix: check decentraland foundation addresses payload (#586)

--- src/hooks/useDecentralandFoundationAddresses.ts
+++ src/hooks/useDecentralandFoundationAddresses.ts
@@ -1,3 +1,5 @@
+import { useMemo } from "react"
+
 import useFeatureFlagContext from "decentraland-gatsby/dist/context/FeatureFlag/useFeatureFlagContext"
 
 import { Flags } from "../modules/features"
@@ -5,12 +7,15 @@ import { Flags } from "../modules/features"
 export function useDecentralandFoundationAddresses() {
   const [ff] = useFeatureFlagContext()
   const isFoundationAddressesEnabled = ff.flags[Flags.DCLFoundationAddresses]
-  const payload = ff.variants[Flags.DCLFoundationAddresses].payload
-  if (isFoundationAddressesEnabled && payload) {
-    return JSON.parse(payload.value).map((address: string) =>
-      address.toLowerCase()
-    )
-  }
+  const payload = ff.variants[Flags.DCLFoundationAddresses]?.payload
+  const addresses = useMemo(() => {
+    if (isFoundationAddressesEnabled && payload) {
+      return JSON.parse(payload.value).map((address: string) =>
+        address.toLowerCase()
+      )
+    }
+    return []
+  }, [payload, isFoundationAddressesEnabled])
 
-  return []
+  return addresses
 }
