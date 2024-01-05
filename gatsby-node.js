7fcfd43f9ac075942b7847ffa3a1ec3bf9025a14
Braian Mellor <braianj@gmail.com>
2024-01-05 11:47:11 -0300

    fix: gatsby build

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,15 +3,21 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const sharp = require("sharp")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require("webpack")
 sharp.cache(false)
 sharp.simd(false)
 
 // You can delete this file if you're not using it
-exports.onCreateWebpackConfig = ({ actions, plugins }) => {
+exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
-    // plugins: [plugins.provide({ Buffer: ['buffer/', 'Buffer'] })],
+    plugins: [
+      new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+        resource.request = resource.request.replace(/^node:/, "")
+      }),
+    ],
     resolve: {
       fallback: {
         assert: false,
