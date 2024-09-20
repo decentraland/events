7159728d4e41858973acc071d053b6cb5168187b
Manu <manuponsa@gmail.com>
2024-09-20 15:31:43 -0300

    fix: ensure updated_at field is updated properly (#687)
    * fix: ensure updated_at field is updated properly
    
    * chore: linter
    
    * fix: local.json events api url

--- src/entities/Event/model.ts
+++ src/entities/Event/model.ts
@@ -17,8 +17,6 @@ import isEthereumAddress from "validator/lib/isEthereumAddress"
 
 import { utils } from "decentraland-commons"
 
-import EventAttendee from "../EventAttendee/model"
-import { ProfileSettingsAttributes } from "../ProfileSettings/types"
 import {
   DeprecatedEventAttributes,
   EventAttributes,
@@ -27,6 +25,8 @@ import {
   SITEMAP_ITEMS_PER_PAGE,
   SessionEventAttributes,
 } from "./types"
+import EventAttendee from "../EventAttendee/model"
+import { ProfileSettingsAttributes } from "../ProfileSettings/types"
 
 export default class EventModel extends Model<DeprecatedEventAttributes> {
   static tableName = "events"
@@ -75,21 +75,28 @@ export default class EventModel extends Model<DeprecatedEventAttributes> {
       throw new Error(`Missing update conditions`)
     }
 
+    const updatedChanges = {
+      ...changes,
+      updated_at: new Date(),
+    }
+
     const sql = SQL`
-      UPDATE ${table(this)}
-      SET
-        ${join(
-          changesKeys.map((key) => SQL`"${SQL.raw(key)}" = ${changes[key]}`),
-          SQL`,`
-        )}
-      WHERE
-        ${join(
-          conditionsKeys.map(
-            (key) => SQL`"${SQL.raw(key)}" = ${conditions[key]}`
-          ),
-          SQL`,`
-        )}
-    `
+    UPDATE ${table(this)}
+    SET
+      ${join(
+        Object.keys(updatedChanges).map(
+          (key) => SQL`"${SQL.raw(key)}" = ${updatedChanges[key]}`
+        ),
+        SQL`,`
+      )}
+    WHERE
+      ${join(
+        conditionsKeys.map(
+          (key) => SQL`"${SQL.raw(key)}" = ${conditions[key]}`
+        ),
+        SQL`,`
+      )}
+  `
 
     return this.query(sql) as any
   }
