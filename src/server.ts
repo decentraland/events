115f9044946836f03912d898ac95c4206d09d156
Gabriel Díaz <git@subsme.slmail.me>
2025-05-28 09:04:37 -0300

    chore: Add vercel to cors (#827)
    * chore: Add vercel to cors
    
    * fix: regex

--- src/server.ts
+++ src/server.ts
@@ -49,6 +49,7 @@ app.use("/api", [
     corsOrigin: [
       /^http:\/\/localhost:[0-9]{1,10}$/,
       /^https:\/\/(.{1,50}\.)?decentraland\.(zone|today|org)$/,
+      /https:\/\/([a-zA-Z0-9\-_])+-decentraland1\.vercel\.app/,
       "https://mvfw.org",
       "https://dcl-metrics.com",
     ],
@@ -80,6 +81,7 @@ app.use("/events", [
     corsOrigin: [
       /^http:\/\/localhost:[0-9]{1,10}$/,
       /^https:\/\/(.{1,50}\.)?decentraland\.(zone|today|org)$/,
+      /https:\/\/([a-zA-Z0-9\-_])+-decentraland1\.vercel\.app/,
       "https://mvfw.org",
       "https://dcl-metrics.com",
     ],
