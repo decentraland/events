d509fd84c868bd6ee2c2feb3cad59ded182c84b3
LautaroPetaccio <lausuper@gmail.com>
2026-04-07 19:21:06 -0300

    fix: Improve build time

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   coverageProvider: "v8",
-  testPathIgnorePatterns: ["/lib/"],
+  testPathIgnorePatterns: ["/lib/", "<rootDir>/test/"],
   moduleNameMapper: {
     "^@dcl/ui-env$": "<rootDir>/src/__mocks__/@dcl/ui-env.ts",
     "^@dcl/ui-env/(.*)$": "<rootDir>/src/__mocks__/@dcl/ui-env.ts",
